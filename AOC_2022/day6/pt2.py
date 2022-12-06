5159c3440d4019b9dd8d40d98b269a552a026ae9
WDominic <dom0_o@protonmail.com>
2022-12-06 00:33:17 -0500

    day 6 python

---
+++ AOC_2022/day6/pt2.py
@@ -0,0 +1,37 @@
+if __name__ == "__main__":
+    f = open("input.txt").read().splitlines()
+    data = []
+    for i in f:  
+        data.append(i)
+
+    buff = data[0]
+
+    view = []
+    visited = {} #letter : freq
+    count = 0
+    for c in buff:
+        if c in visited:
+            visited[c] += 1
+        else:
+            visited[c] = 1
+        view.append(c)
+        if len(view) == 15:
+            visited[view[0]] -= 1
+            if visited[view[0]] == 0: # remove entry
+                visited.pop(view[0])
+            view.pop(0)
+        print(view)
+        count += 1
+
+        if len(visited) == 14:
+            boo = True
+            for i in visited.values():
+                if i > 1:
+                    boo = False
+                    break
+            if(boo):
+                print(count)
+                break
+        
+        
+        
