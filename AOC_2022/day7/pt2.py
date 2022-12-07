395296ebc35eede3b05d42672f7ba1542d4552bb
W-Dominic <dom0_o@protonmail.com>
2022-12-07 13:30:31 -0500

    day 7 pt2 python

--- AOC_2022/day7/pt2.py
+++ AOC_2022/day7/pt2.py
@@ -11,65 +11,54 @@ class Directory:
     def set_parent(self, dir):
         self.parent = dir
 
+def adjust_size(node, size):
+    if node.parent == "":
+        return
+    node.parent.size += size
+    adjust_size(node.parent, size)
+
 if __name__ == "__main__":
-    f = open("test_data.txt").read().splitlines()
+    f = open("input.txt").read().splitlines()
     data = []
     for i in f:  
         data.append(i)
 
     root = Directory("/")
     current_dir = root
-    total_size = 0
     for d in data:
         line = d.split(" ")
         if line[0] == "$": #command
             if line[1] == "cd":
                 if line[2] == "..":
                     current_dir = current_dir.parent
-                    print("changing directory to", current_dir.name)
                 else:
                     for dir in current_dir.children:
                         if dir.name == line[2]:
                             current_dir = dir
-                            print("changing directory to", current_dir.name)
                             break
         else: #output, means we just executed an ls
-            if (current_dir.size == 0):
-                if line[0] == "dir": # means we need to add child
-                    child = Directory(line[1])
-                    child.parent = current_dir
-                    current_dir.add_child(child)
-                else: # we have a file
-                    current_dir.increase_size(int(line[0]))
-                    total_size += int(line[0])
+            if line[0] == "dir": # means we need to add child
+                child = Directory(line[1])
+                child.parent = current_dir
+                current_dir.add_child(child)
+            else: # we have a file, increase current_dir size and adjust its parents size
+                current_dir.increase_size(int(line[0]))
+                adjust_size(current_dir, int(line[0]))
 
     #bfs traversal of directories
-    global needs_adjust
-    needs_adjust = []
-    def dfs(curr):
-        global needs_adjust
-        if len(curr.children) == 0:
-            needs_adjust.append(curr)
-            return
-        for i in curr.children:
-            dfs(i)
-    dfs(root)
-
-    q = needs_adjust
+    global sizes
     sizes = []
-    while(len(q) > 0):
-        curr = needs_adjust.pop(0)
-        sizes.append(curr.size)
-        if curr.parent != "":
-            curr.parent.increase_size(curr.size)
-            if not curr.parent in q:
-                q.append(curr.parent)
-
-    print(sizes)
-    print(root.size, total_size)
-    space_still_needed = 30000000 - (70000000 - total_size)
+    def dfs(node):
+        if len(node.children) == 0:
+            return
+        for i in node.children:
+            sizes.append(i.size)
+            dfs(i)
+    
+    dfs(root)
+    space_still_needed = 30000000 - (70000000 - root.size)
     potential = [i for i in sizes if i >= space_still_needed]
-
     print(min(potential))
+
     
 
